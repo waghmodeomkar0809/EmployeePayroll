c485542afbb70c7b1d8a041844bf7abe79def322
waghmodeomkar0809 <waghmodeomkar975@gmail.com>
2022-10-10 10:11:34 +0530

    Add CreateTable

--- PayrollService.sql
+++ PayrollService.sql
@@ -1,3 +1,13 @@
 create database Payroll_service
 
 use Payroll_service
+
+create table Employee_Payroll
+(
+Id int,
+Name varchar(255),
+Salary varchar(255),
+start date 
+)
+
+select * from  Employee_Payroll

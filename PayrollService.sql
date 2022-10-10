c72b4642ad61a8c1aec003022b295b9e7cc91ed3
waghmodeomkar0809 <waghmodeomkar975@gmail.com>
2022-10-10 11:06:19 +0530

    Add SelectEmployeePayroll

--- PayrollService.sql
+++ PayrollService.sql
@@ -10,7 +10,9 @@ Salary varchar(255),
 start date 
 )
 
-select * from  Employee_Payroll
+
 
 insert into Employee_Payroll(Id,Name,Salary,start)values
 (2,'Omkar',20000,'10-10-2022'),(5,'karan',30000,'11-10-2022')
+
+select * from  Employee_Payroll
